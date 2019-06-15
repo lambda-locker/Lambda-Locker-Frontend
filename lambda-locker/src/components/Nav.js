4f55dd8fefb3bf88967fb79dfa20b840c60fcd02
blubrandi <blubrandi0@gmail.com>
2019-06-15 14:21:38 -0600

    Added logo file to nav, added styles for logo in Nav component.

--- lambda-locker/src/components/Nav.js
+++ lambda-locker/src/components/Nav.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 import { NavLink } from 'react-router-dom'
 
-import button from '../css/button.css'
+import Logo from '../assets/lambda-locker-logo-transparent-white.png'
+
+import '../css/button.css'
 import styled from 'styled-components'
 
 const NavBar = styled.div`
@@ -11,20 +13,35 @@ const NavBar = styled.div`
     display: flex;
     justify-content: center;
 
-    .navContainer {
+    .linkContainer {
         width: 80%;
         padding: 20px 0;
         display: flex;
         justify-content: flex-end;
         max-height: 60px;        
     }
+
+    .brandContainer {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+
+        img {
+            max-width: 120px;
+        }
+    }
+
+
 `
 
 const Nav = () => {
 
     return (
         <NavBar>
-            <div className="navContainer">
+            <div className="brandContainer">
+                <img src={Logo} />
+            </div>
+            <div className="linkContainer">
                 <NavLink to='/register'><button>Register</button></NavLink>
                 <NavLink to='/login'><button>Login</button></NavLink>
             </div>
