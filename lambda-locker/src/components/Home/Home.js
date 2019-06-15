59cf90877badc54af71cc95a6905ad4a1e6bd655
blubrandi <blubrandi0@gmail.com>
2019-06-15 17:10:53 -0600

    Added intro text to Home component

--- lambda-locker/src/components/Home/Home.js
+++ lambda-locker/src/components/Home/Home.js
@@ -1,11 +1,30 @@
 import React from 'react'
 
-const Home = () => {
+import styled from 'styled-components'
 
+const HomeContainer = styled.div`
+    width: 420px;
+    margin: 0 auto;
+
+    p {
+        font-size: 1.2rem;
+        text-align: left;
+    }
+
+    h2 {
+        text-align: left;
+    }
+`
+
+const Home = () => {
     return (
-        <div>
-            <h2>This is our react app...</h2>
-        </div>
+        <HomeContainer>
+            <h1>Welcome to <span className="lambdaText">Lambda Locker</span></h1>
+            <p><span className="lambdaText">Lambda Locker</span> is your personal space to store notes, links, and resources as you journey through the <span className="lambdaText">Lambda</span>  Curriculum.</p>
+            <p>When you create your <span className="lambdaText">Lambda Locker</span>, you'll also have access to the hundreds of resources other <span className="lambdaText">Lambda </span>students have stored and found useful themselves.</p>
+            <h2>Interested in learning more?</h2>
+            <p>Discover <span className="lambdaText">Lambda Locker</span>.</p>
+        </HomeContainer>
     )
 }
 
